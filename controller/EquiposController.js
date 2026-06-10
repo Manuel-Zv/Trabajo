56ad3fc143ccdcd9b2fe4435adda7fff12f7842e
Emilio Zambrano <emiliozambranovite@gmail.com>
2026-06-10 15:43:39 -0500

    get id

--- controller/EquiposController.js
+++ controller/EquiposController.js
@@ -10,3 +10,18 @@ export const GetallEquipos = async (req, res) => {
         res.status(500).json({ error: 'Error al obtener los equipos' });
     }
 };
+export const GetEquipoById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const equipo = await Equipos.findByPk(id);
+        if (equipo) {
+            res.json(equipo);
+        } else {
+            res.status(404).json({ error: 'Equipo no encontrado' });
+        }
+    }
+    catch (error) {
+        console.error(`Error ${error}`);
+        res.status(500).json({ error: 'Error al obtener el equipo' });
+    }
+};
